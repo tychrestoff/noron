4bec87af4f8707faae46d5456b8f1ce8e803e3f0
Tyler Chrestoff <TChrestoff@VillageCommunities.com>
2016-04-09 14:02:28 -0400

    Last commit in Tensor branch. Changing to NdArray.

--- Noron/Noron/Tensors/Tensor.cs
+++ Noron/Noron/Tensors/Tensor.cs
@@ -1,6 +1,28 @@
-﻿namespace Noron.Tensors
+﻿using System;
+using Noron.Utilities;
+namespace Noron.Tensors
 {
     public class Tensor
     {
+        public Tensor(int[] dimensions)
+        {
+            Dimensions = dimensions;
+            Size = dimensions.Product();
+            Data = new double[Size];
+        }
+
+        public double[] Data { get; }
+        public int[] Dimensions { get; private set; }
+        public int Rank => Dimensions.Length;
+        public int Size { get; }
+
+        public void Reshape(int[] newDimensions)
+        {
+            var newSize = newDimensions.Product();
+
+            if (Size != newSize) throw new ArgumentException(
+                "Invalid reshape size.");
+            Dimensions = newDimensions;
+        }
     }
 }
