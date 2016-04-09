604f37ffbdc13395afb74d3e55f7d1c67329dc23
Tyler Chrestoff <TChrestoff@VillageCommunities.com>
2016-04-09 16:14:01 -0400

    Added NdArray class supporting unary and binary methods, other helpful functions

---
+++ Noron/Noron/Utilities/NoronUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using static System.Math;
+
+namespace Noron.Utilities
+{
+    public static class NoronUtilities
+    {
+        public static double Acosh(double x)
+            => Log(x + Sqrt(Pow(x, 2) - 1));
+        public static double Asinh(double x)
+            => Log(x + Sqrt(Pow(x, 2) + 1));
+        public static double Atanh(double x)
+            => 0.5 * Log((1 + x) / (1 - x));
+        public static double Sigmoid(double x)
+            => 1 / (1 + Exp(-x));
+    }
+}
